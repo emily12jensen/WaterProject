939214f338c5874b58ecfad9cbe8abf11a927d4b
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-03-03 23:27:03 -0700

    update

--- Models/Basket.cs
+++ Models/Basket.cs
@@ -9,7 +9,7 @@ namespace WaterProject.Models
     {
         public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
 
-        public void AddItem(Project proj, int qty)
+        public virtual void AddItem(Project proj, int qty)
         {
             BasketLineItem line = Items
                 .Where(p => p.Project.ProjectId == proj.ProjectId)
@@ -27,6 +27,16 @@ namespace WaterProject.Models
             {
                 line.Quantity += qty;
             }
+        }
+
+        public virtual void RemoveItem(Project proj)
+        {
+            Items.RemoveAll(x => x.Project.ProjectId == proj.ProjectId);
+
+        }
+        public virtual void ClearBasket()
+        {
+            Items.Clear();
         }
         public double CalculateTotal()
         {
--- Pages/Donate.cshtml.cs
+++ Pages/Donate.cshtml.cs
@@ -12,29 +12,35 @@ namespace WaterProject.Pages
     public class DonateModel : PageModel
     {
         private IWaterProjectRepository repo { get; set; }
-        public DonateModel(IWaterProjectRepository temp)
+        public DonateModel(IWaterProjectRepository temp, Basket b)
         {
             repo = temp;
+            basket = b;
         }
         public Basket basket { get; set; }
         public string ReturnUrl {get; set;}
         public void OnGet(string returnUrl)
         {
-            returnUrl = returnUrl ?? "/";
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            ReturnUrl = returnUrl ?? "/";
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
 
         public IActionResult OnPost(int projectId, string returnUrl)
         {
 
             Project p = repo.Projects.FirstOrDefault(x => x.ProjectId == projectId);
-            basket = HttpContext.Session.GetJson<Basket>("basket") ??  new Basket();
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ??  new Basket();
             basket.AddItem(p, 1);
 
-            HttpContext.Session.SetJson("basket", basket);
+            // HttpContext.Session.SetJson("basket", basket);
 
             return RedirectToPage(new { ReturnUrl = returnUrl });
 
+        }
+        public IActionResult OnPostRemove(int projectId, string returnUrl)
+        {
+            basket.RemoveItem(basket.Items.First(x => x.Project.ProjectId == projectId).Project);
+            return RedirectToPage( new {ReturnUrl = returnUrl});
         }
     }
 }
